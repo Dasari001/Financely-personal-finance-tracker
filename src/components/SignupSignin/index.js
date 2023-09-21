41546d79bf99e994a1fafe82cf95610034ad4ef2
Dasari001 <dasarisaibabu01@gmail.com>
2023-09-21 13:49:17 +0530

    Financely

--- src/components/SignupSignin/index.js
+++ src/components/SignupSignin/index.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
+import Input from '../Input'
+import Button from '../Button'
 
 const SignupSigninComponent = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
   return (
-    <div className='signup-wrapper'>SignupSigninComponent</div>
+    <div className='signup-wrapper'>
+      <h2 className='title'>Sign Up on <span style={{ color: "var(--theme)"}}>Financely.</span></h2>
+      <form>
+         <Input
+          label={"full Name"} 
+          state={name} 
+          setState={setName} 
+          placeholder={"John Doe"}
+        />
+        <Input
+          label={"Email"} 
+          state={email} 
+          setState={setEmail} 
+          placeholder={"JohnDoe@gmail.com"}
+        />
+        <Input
+          label={"Password"} 
+          state={password} 
+          setState={setPassword} 
+          placeholder={"Example123"}
+        />
+        <Input
+          label={"Confirm Password"} 
+          state={confirmPassword} 
+          setState={setConfirmPassword} 
+          placeholder={"Example123"}
+        />
+      </form>
+      <Button text={"Signup using Email and Password"}/>
+      <p style={{textAlign: "center", margin: 0}}>or</p>
+      <Button text={"Signup Using Google"} blue={true}/>
+    </div>
   )
 }
 
